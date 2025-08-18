fdc45f60a2e79403f4a76a48ab64a91740bf9154
Nitin Dahyabhai <thatnitind@gmail.com>
2025-08-18 00:20:15 -0400

    Increment bundle version for 3.39

--- web/bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/contentmodel/TaglibController.java
+++ web/bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/contentmodel/TaglibController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2021 IBM Corporation and others.
+ * Copyright (c) 2001, 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -155,7 +155,7 @@ public class TaglibController implements IDocumentSetupParticipant, IDocumentSet
 		 */
 		public void bufferDisposed(IFileBuffer buffer) {
 			DocumentInfo info;
-			
+
 			if (buffer instanceof ITextFileBuffer) {
 				IDocument document = ((ITextFileBuffer) buffer).getDocument();
 				synchronized (_instance.fJSPdocuments) {
